492ee5cc51b7f0464490f9071a4862aab5413a6e
Dolly <285661571@qq.com>
2017-08-09 00:17:03 +0800

    Signed-off-by: Dolly <285661571@qq.com>

--- app/components/controller/bookList.js
+++ app/components/controller/bookList.js
@@ -7,32 +7,34 @@ angular
         $scope.books= [];
         $scope.filterText = "";
         $scope.pagingOptions = {
-            pageSize: 3,
+            pageSize: 2,
             currentPage: 1,
             pageLength: 1
         };
         $scope.getDataAsync = function(pageSize, page, searchText) {
-            var data;
             if (searchText) {
                 var ft = searchText.toLowerCase();
-                $http.get('../data/books.json')
+                $http.get('http://localhost:3001/books/'+page+'/'+pageSize+'/'+searchText)
                     .success(function(largeLoad) {
-                        data = largeLoad.filter(function(item) {
+                        $scope.books = largeLoad.books;
+                        $scope.pagingOptions.pageLength = (largeLoad.pages).toString();
+                        /*data = largeLoad.books.filter(function(item) {
                             return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
                         });
-                        $scope.setPagingData(data, page, pageSize);
+                        $scope.setPagingData(data, page, pageSize);*/
                     });
             } else {
-                $http.get('../data/books.json')
+                $http.get('http://localhost:3001/books/'+page+'/'+pageSize)
                     .success(function(largeLoad) {
-                        $scope.setPagingData(largeLoad, page, pageSize);
+                        $scope.books = largeLoad.books;
+                        $scope.pagingOptions.pageLength = (largeLoad.pages).toString();
                     });
             }
         };
 
         $scope.setPagingData = function(data, page, pageSize) {
-            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-            $scope.books = pagedData;
+            //var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.books = data;
             $scope.pagingOptions.pageLength = Math.ceil(data.length/pageSize).toString();
             if (!$scope.$$phase) { //返回$digest
                 $scope.$apply();
--- app/dist/js/bundle.js
+++ app/dist/js/bundle.js
@@ -18734,30 +18734,32 @@ angular.module('bookList', []).controller('bookList', ['$scope', '$http', '$time
     $scope.books = [];
     $scope.filterText = "";
     $scope.pagingOptions = {
-        pageSize: 3,
+        pageSize: 2,
         currentPage: 1,
         pageLength: 1
     };
     $scope.getDataAsync = function (pageSize, page, searchText) {
-        var data;
         if (searchText) {
             var ft = searchText.toLowerCase();
-            $http.get('../data/books.json').success(function (largeLoad) {
-                data = largeLoad.filter(function (item) {
+            $http.get('http://localhost:3001/books/' + page + '/' + pageSize + '/' + searchText).success(function (largeLoad) {
+                $scope.books = largeLoad.books;
+                $scope.pagingOptions.pageLength = largeLoad.pages.toString();
+                /*data = largeLoad.books.filter(function(item) {
                     return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
                 });
-                $scope.setPagingData(data, page, pageSize);
+                $scope.setPagingData(data, page, pageSize);*/
             });
         } else {
-            $http.get('../data/books.json').success(function (largeLoad) {
-                $scope.setPagingData(largeLoad, page, pageSize);
+            $http.get('http://localhost:3001/books/' + page + '/' + pageSize).success(function (largeLoad) {
+                $scope.books = largeLoad.books;
+                $scope.pagingOptions.pageLength = largeLoad.pages.toString();
             });
         }
     };
 
     $scope.setPagingData = function (data, page, pageSize) {
-        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-        $scope.books = pagedData;
+        //var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+        $scope.books = data;
         $scope.pagingOptions.pageLength = Math.ceil(data.length / pageSize).toString();
         if (!$scope.$$phase) {
             //返回$digest
